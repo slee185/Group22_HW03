e14c15b56829306bb25ed18cbfed94262ef4674c
Stephanie <slee185@uncc.edu>
2022-09-16 13:36:30 -0400

    started ViewDrinksFragment

--- app/src/main/java/com/example/group22_hw03/fragments/BACCalculatorFragment.java
+++ app/src/main/java/com/example/group22_hw03/fragments/BACCalculatorFragment.java
@@ -50,7 +50,6 @@ public class BACCalculatorFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_baccalculator, container, false);
-
     }
 
     @Override
--- app/src/main/java/com/example/group22_hw03/fragments/ViewDrinksFragment.java
+++ app/src/main/java/com/example/group22_hw03/fragments/ViewDrinksFragment.java
@@ -10,13 +10,31 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import com.example.group22_hw03.Drink;
 import com.example.group22_hw03.R;
 
+import java.util.ArrayList;
+
 public class ViewDrinksFragment extends Fragment {
+
+    TextView viewAlcoholPercent;
+    TextView viewCurrentDrinkNumber;
+    TextView viewDateAdded;
+    TextView viewDrinkSize;
+    Button viewDrinksButtonClose;
+    ImageButton viewDrinksButtonTrash;
+    ImageButton viewDrinksButtonPrevious;
+    ImageButton viewDrinksButtonNext;
+
+    int currentDrinkNumber = 0;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,6 +48,22 @@ public class ViewDrinksFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        viewAlcoholPercent = view.findViewById(R.id.viewAlcoholPercent);
+        viewCurrentDrinkNumber = view.findViewById(R.id.viewCurrentDrinkNumber);
+        viewDateAdded = view.findViewById(R.id.viewDateAdded);
+        viewDrinkSize = view.findViewById(R.id.viewDrinkSize);
+        viewDrinksButtonClose = view.findViewById(R.id.viewDrinksButtonClose);
+        viewDrinksButtonTrash = view.findViewById(R.id.viewDrinksButtonTrash);
+        viewDrinksButtonPrevious = view.findViewById(R.id.viewDrinksButtonPrevious);
+        viewDrinksButtonNext = view.findViewById(R.id.viewDrinksButtonNext);
+
+        // ArrayList<Drink> drinks =
+
+        view.findViewById(R.id.viewDrinksButtonTrash).setOnClickListener(v -> {
+
+        });
+
     }
 
     @Override
